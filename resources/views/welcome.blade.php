7bac929de6bc822355b408777fe24fe57abd75df
davidhalasz <davidhalasz22@gmail.com>
2022-09-02 20:36:10 +0200

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,227 @@
+<x-guest-layout>
+    <div class="bg-blue-500">
+        <div class="max-w-2xl mx-auto headerDiv bg-blue-500 text-white">
+            <h1 class="text-6xl font-bold text-center pt-20">BIOALARM</h1>
+            <p class="pt-4 text-lg font-bold text-center">Biológiai alapú vízminőség állapot felmérő és riasztó rendszer
+                megvalósítása, feltételeinek kidolgozása.</p>
+        </div>
+    </div>
+    <div>
+        <svg id="wave" style="transform:rotate(180deg); transition: 0.3s" viewBox="0 0 1440 330" version="1.1"
+            xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+                    <stop stop-color="rgba(59, 130, 246, 1)" offset="0%"></stop>
+                    <stop stop-color="rgba(147, 197, 253, 1)" offset="100%"></stop>
+                </linearGradient>
+            </defs>
+            <path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)"
+                d="M0,132L80,143C160,154,320,176,480,176C640,176,800,154,960,148.5C1120,143,1280,154,1440,154C1600,154,1760,143,1920,154C2080,165,2240,198,2400,209C2560,220,2720,209,2880,192.5C3040,176,3200,154,3360,170.5C3520,187,3680,242,3840,220C4000,198,4160,99,4320,93.5C4480,88,4640,176,4800,225.5C4960,275,5120,286,5280,258.5C5440,231,5600,165,5760,115.5C5920,66,6080,33,6240,66C6400,99,6560,198,6720,236.5C6880,275,7040,253,7200,231C7360,209,7520,187,7680,170.5C7840,154,8000,143,8160,148.5C8320,154,8480,176,8640,154C8800,132,8960,66,9120,82.5C9280,99,9440,198,9600,198C9760,198,9920,99,10080,93.5C10240,88,10400,176,10560,198C10720,220,10880,176,11040,165C11200,154,11360,176,11440,187L11520,198L11520,330L11440,330C11360,330,11200,330,11040,330C10880,330,10720,330,10560,330C10400,330,10240,330,10080,330C9920,330,9760,330,9600,330C9440,330,9280,330,9120,330C8960,330,8800,330,8640,330C8480,330,8320,330,8160,330C8000,330,7840,330,7680,330C7520,330,7360,330,7200,330C7040,330,6880,330,6720,330C6560,330,6400,330,6240,330C6080,330,5920,330,5760,330C5600,330,5440,330,5280,330C5120,330,4960,330,4800,330C4640,330,4480,330,4320,330C4160,330,4000,330,3840,330C3680,330,3520,330,3360,330C3200,330,3040,330,2880,330C2720,330,2560,330,2400,330C2240,330,2080,330,1920,330C1760,330,1600,330,1440,330C1280,330,1120,330,960,330C800,330,640,330,480,330C320,330,160,330,80,330L0,330Z">
+            </path>
+            <defs>
+                <linearGradient id="sw-gradient-1" x1="0" x2="0" y1="1" y2="0">
+                    <stop stop-color="rgba(59, 130, 246, 1)" offset="0%"></stop>
+                    <stop stop-color="rgba(59, 130, 246, 1)" offset="100%"></stop>
+                </linearGradient>
+            </defs>
+            <path style="transform:translate(0, 50px); opacity:0.9" fill="url(#sw-gradient-1)"
+                d="M0,132L80,115.5C160,99,320,66,480,71.5C640,77,800,121,960,137.5C1120,154,1280,143,1440,115.5C1600,88,1760,44,1920,44C2080,44,2240,88,2400,121C2560,154,2720,176,2880,187C3040,198,3200,198,3360,187C3520,176,3680,154,3840,121C4000,88,4160,44,4320,44C4480,44,4640,88,4800,104.5C4960,121,5120,110,5280,99C5440,88,5600,77,5760,82.5C5920,88,6080,110,6240,148.5C6400,187,6560,242,6720,269.5C6880,297,7040,297,7200,247.5C7360,198,7520,99,7680,93.5C7840,88,8000,176,8160,176C8320,176,8480,88,8640,44C8800,0,8960,0,9120,38.5C9280,77,9440,154,9600,170.5C9760,187,9920,143,10080,132C10240,121,10400,143,10560,137.5C10720,132,10880,99,11040,82.5C11200,66,11360,66,11440,66L11520,66L11520,330L11440,330C11360,330,11200,330,11040,330C10880,330,10720,330,10560,330C10400,330,10240,330,10080,330C9920,330,9760,330,9600,330C9440,330,9280,330,9120,330C8960,330,8800,330,8640,330C8480,330,8320,330,8160,330C8000,330,7840,330,7680,330C7520,330,7360,330,7200,330C7040,330,6880,330,6720,330C6560,330,6400,330,6240,330C6080,330,5920,330,5760,330C5600,330,5440,330,5280,330C5120,330,4960,330,4800,330C4640,330,4480,330,4320,330C4160,330,4000,330,3840,330C3680,330,3520,330,3360,330C3200,330,3040,330,2880,330C2720,330,2560,330,2400,330C2240,330,2080,330,1920,330C1760,330,1600,330,1440,330C1280,330,1120,330,960,330C800,330,640,330,480,330C320,330,160,330,80,330L0,330Z">
+            </path>
+        </svg>
+    </div>
+    <div class="container mx-auto pb-6">
+        <h1 class="text-3xl pb-4 text-center">
+            BIOALARM Kft
+        </h1>
+        <div class="grid grid-cols-2">
+            <p class="my-auto text-lg">
+                A BIOALARM Kft. 2013 májusában, az Észak-Magyarországi Régióban, Bánki székhellyel alakult innovatív
+                start-up vállalkozás. A cég létrejöttének célja a Bánki-tó élővilágának vizsgálatán alapuló innovatív
+                ötlet
+                megvalósítása, egy biológiai alapú vízminőség állapot felmérő és riasztó rendszer megvalósíthatósága
+                feltételeit összefoglaló tanulmány kidolgozása. A vállalkozás a feltételrendszer dokumentálását követően
+                kockázati tőke bevonásával olyan terméket szeretne előállítani, amely nem csak Magyarországon, hanem
+                Európa-szerte értékesíthető és használható a természetes vizek minőségének vizsgálatára.
+            </p>
+            <img class="mx-auto" style="height: 400px" src="{{ asset('images/kep1.png') }}" alt="">
+        </div>
+    </div>
+
+    <div class="bg-blue-200 pb-6">
+        <div class="container mx-auto">
+            <h1 class="text-3xl text-white text-center font-bold pt-10 pb-10">
+                A projekt
+            </h1>
+            <div class="grid grid-cols-2 gap-10">
+                <div class="bg-slate-900 text-white p-8 rounded-3xl relative mb-6">
+                    <div class="bg-blue-500 w-14 h-14 p-4 rounded-full font-bold absolute -top-4 -right-4 text-center ">
+                        <span>
+                            1.
+                        </span>
+                    </div>
+
+                    <p class="text-lg">
+                        A projektben egy olyan ötlet kerül megvalósíthatósági tanulmánya kerül kidolgozásra, mely első,
+                        kutatási
+                        fázisában feltérképezésre kerülnek a vízminőség vizsgálat során Európában alkalmazott,
+                        minősített
+                        biológiai módszerek. A kutatás adatgyűjtésből, összehasonlító elemzésből, értékelésből áll. Ezt
+                        követően
+                        kerül sor a megvizsgált módszerek közül annak kiválasztására, melyből kinyert egyes adatok
+                        megfelelő
+                        alapot nyújthatnak egy szoftver kifejlesztésére. A szelektálás fő szempontja olyan, a Bánki-tóra
+                        leginkább alkalmazható akkreditált módszer meghatározása, amelyből érkező adatok egy szoftver
+                        inputjaként szolgálhatnak. Matematikai modellt állítva a leghatékonyabb úton feldolgozva és
+                        értékelve a
+                        bemenő adatokat állapíthatják meg a mintavételezés helyszínén, egy referencia mintához képest
+                        történő
+                        eltéréseit, a víz minőségének változását. Az erre vonatkozó szabályozás szerinti határértékek
+                        figyelésével, a vízminőség romlása esetén, adott mutatók küszöbértékeinek meghaladásakor
+                        riasztást
+                        lesz
+                        képes küldeni a rendszer meghatározott módon, meghatározott személyeknek, illetve előzetesen
+                        definiált
+                        automatikus akciót indíthat be.
+                    </p>
+                </div>
+                <div>
+                    <div class="bg-slate-900 text-white p-8 rounded-3xl relative mb-6">
+                        <div
+                            class="bg-blue-500 w-14 h-14 p-4 rounded-full font-bold absolute -top-4 -left-4 text-center">
+                            <span>
+                                2.
+                            </span>
+                        </div>
+
+                        <p class="text-lg">
+                            A megvalósíthatósági tanulmány második fázisában az előállítandó rendszer szoftverének
+                            rendszerterv
+                            készül el. Ez egy döntéstámogató szakértői rendszer lesz, amely alkalmas a biológiai
+                            paraméterek
+                            mérése
+                            során érkező adatok alapján, azok kiértékelésével a vízminőség meghatározására. Jelentős
+                            vízminőség
+                            romlás esetén SMS, e-mail, egyéb elektronikai riasztórendszer segítségével riasztások,
+                            automatikus
+                            adekvát védelmi mechanizmusok generálására elsőként a megvalósítás helyszínen, Bánkon
+                            (Bánki-tó,
+                            környező élővizek), mint pilot környezetben, majd az Észak-Magyarországi Régió természetes
+                            vizeiben,
+                            valamint miképpen terjeszthető ki üzleti alapon a rendszer Magyarország más régióira,
+                            illetve a
+                            határainkon kívülre is.
+                        </p>
+                    </div>
+
+                    <div class="mx-auto w-fit bg-slate-900 text-white p-8 rounded-3xl mb-6">
+                        <table class="table-auto">
+                            <tbody>
+                                <tr>
+                                    <td class="pr-4">Projektazonosító:</td>
+                                    <td>ÉMOP-1.3.1-12-2012-0053</td>
+                                </tr>
+                                <tr>
+                                    <td class="pr-4">Projekt összköltsége:</td>
+                                    <td>9.000.000 Ft</td>
+                                </tr>
+                                <tr>
+                                    <td class="pr-4">Támogatás:</td>
+                                    <td>9.000.000 Ft</td>
+                                </tr>
+                                <tr>
+                                    <td class="pr-4">Megvalósítás éve:</td>
+                                    <td>2013-2014</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container mx-auto py-12">
+        <h1 class="text-3xl pb-6 text-center">
+            Partnerek
+        </h1>
+        <div class="grid grid-cols-3">
+            <img class="m-auto" src="{{ asset('images/logo_regi_font2.png') }}" alt="Norria">
+            <img class="m-auto max-h-16" src="{{ asset('images/dbh_Innohub_logo-1234x556.png') }}" alt="Norria">
+            <p class="m-auto text-2xl">Zaniotech Kft.</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto py-12">
+        <h1 class="text-3xl pb-6 text-center">
+            Hírek, események
+        </h1>
+
+        <div class="grid grid-cols-3 gap-4">
+            <div class="border border-blue-500 hover:bg-blue-500 hover:text-white p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <h2 class="text-lg pb-3">Lorem ipsum dolor et cetera</h2>
+                <p class="line-clamp-3">Nulla facilisi. Maecenas faucibus varius gravida. Morbi sodales, justo vel
+                    tempor
+                    vestibulum, nisl metus eleifend nunc, vitae dignissim sapien turpis id ex. Proin in pharetra quam,
+                    vitae sodales tortor. Morbi libero dolor, egestas faucibus fermentum quis, feugiat at metus. Nunc ac
+                    justo sapien.</p>
+            </div>
+            <div class="border border-blue-500 hover:bg-blue-500 hover:text-white p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <h2 class="text-lg pb-3">Lorem ipsum dolor et cetera</h2>
+                <p class="line-clamp-3">Nulla facilisi. Maecenas faucibus varius gravida. Morbi sodales, justo vel
+                    tempor
+                    vestibulum, nisl metus eleifend nunc, vitae dignissim sapien turpis id ex. Proin in pharetra quam,
+                    vitae sodales tortor. Morbi libero dolor, egestas faucibus fermentum quis, feugiat at metus. Nunc ac
+                    justo sapien.</p>
+            </div>
+            <div class="border border-blue-500 hover:bg-blue-500 hover:text-white p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <h2 class="text-lg pb-3">Lorem ipsum dolor et cetera</h2>
+                <p class="line-clamp-3">Nulla facilisi. Maecenas faucibus varius gravida. Morbi sodales, justo vel
+                    tempor
+                    vestibulum, nisl metus eleifend nunc, vitae dignissim sapien turpis id ex. Proin in pharetra quam,
+                    vitae sodales tortor. Morbi libero dolor, egestas faucibus fermentum quis, feugiat at metus. Nunc ac
+                    justo sapien.</p>
+            </div>
+            <div class="border border-blue-500 hover:bg-blue-500 hover:text-white p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <h2 class="text-lg pb-3">Lorem ipsum dolor et cetera</h2>
+                <p class="line-clamp-3">Nulla facilisi. Maecenas faucibus varius gravida. Morbi sodales, justo vel
+                    tempor
+                    vestibulum, nisl metus eleifend nunc, vitae dignissim sapien turpis id ex. Proin in pharetra quam,
+                    vitae sodales tortor. Morbi libero dolor, egestas faucibus fermentum quis, feugiat at metus. Nunc ac
+                    justo sapien.</p>
+            </div>
+            <div class="border border-blue-500 hover:bg-blue-500 hover:text-white p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <h2 class="text-lg pb-3">Lorem ipsum dolor et cetera</h2>
+                <p class="line-clamp-3">Nulla facilisi. Maecenas faucibus varius gravida. Morbi sodales, justo vel
+                    tempor
+                    vestibulum, nisl metus eleifend nunc, vitae dignissim sapien turpis id ex. Proin in pharetra quam,
+                    vitae sodales tortor. Morbi libero dolor, egestas faucibus fermentum quis, feugiat at metus. Nunc ac
+                    justo sapien.</p>
+            </div>
+            <div class="border border-blue-500 hover:bg-blue-500 hover:text-white p-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <h2 class="text-lg pb-3">Lorem ipsum dolor et cetera</h2>
+                <p class="line-clamp-3">Nulla facilisi. Maecenas faucibus varius gravida. Morbi sodales, justo vel
+                    tempor
+                    vestibulum, nisl metus eleifend nunc, vitae dignissim sapien turpis id ex. Proin in pharetra quam,
+                    vitae sodales tortor. Morbi libero dolor, egestas faucibus fermentum quis, feugiat at metus. Nunc ac
+                    justo sapien.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto py-12">
+        <h1 class="text-3xl pb-6 text-center">
+            Galéria
+        </h1>
+        <div class="grid grid-cols-6 gap-4">
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+            <div class="h-52 w-52 border border-blue-500"></div>
+        </div>
+    </div>
+
+</x-guest-layout>
